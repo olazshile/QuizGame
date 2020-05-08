f86081a9b0c4b2f2e502c91287e8e1d3461bb857
olazshile <olashilequadry@gmail.com>
2020-05-08 16:47:27 +0100

    adding js file

---
+++ questionbank.js
@@ -0,0 +1,22 @@
+
+const questions = [
+  { question: 'CSS Stands for...',
+  options: ['(A) Computer Styled Sections', '(B) Cascading Style Sheets', '(C) Crazy Solid Shapes', '(D) None of the above'],
+  correctAnswer: 1 },
+
+    { question: 'Who invented JavaScript?',
+      options: ['(A) Douglas Crockford', '(B) Brendan Eich', '(C) Sheril Sanberg', '(D) Mark Zuckerberg'],
+      correctAnswer: 1 },
+
+    { question: 'Which of the following is not an HTML tag',
+      options: ['(A) center', '(B) border', '(C) blockquote', '(D) None of the above'],
+      correctAnswer: 1 },
+ 
+    { question: 'Which one of these is a JavaScript package manager?',
+      options: ['(A) Node.js', '(B) npm', '(C) TypeScript', '(D) LiveScript'],
+      correctAnswer: 1 },
+
+    { question: 'Which of the following is a text Editor?',
+      options: ['(A) Notepad', '(B) Sublime Text', '(C) Chrome', '(D) Phoenix Editor'],
+      correctAnswer: 1 },
+];
---
+++ script.js
@@ -0,0 +1,95 @@
+
+// Get the active question-number a
+let currentQuestion = 0;
+let questionIndex = 0;
+let totalQuestion = questions.length;
+let correctAnswer = 0;
+
+//GEt question available
+const option = document.getElementById('question-option');
+const container = document.getElementById('question-container');
+const questionContent = document.getElementById('question-index');
+let nextQuestion = document.getElementById('nextbtn');
+let resultCounter = document.getElementById('result');
+
+//Get the values in the option box
+const firstopt = document.getElementById('0');
+const secondopt = document.getElementById('1');
+const thirdopt = document.getElementById('2');
+const fourthopt = document.getElementById('3');
+
+// set Question on loading the browser
+function loadQuestion(questionIndex) {
+
+    let q = questions[questionIndex];
+
+  if(questionIndex < questions.length){
+    questionContent.textContent = `${questionIndex+1}. ${q.question}`;
+    firstopt.textContent = q.options[0];
+    secondopt.textContent = q.options[1];
+    thirdopt.textContent = q.options[2];
+    fourthopt.textContent = q.options[3];
+  }else{
+      questionContent.innerHTML = `<h1><center>Congrats! You have completed the Quiz Challenge</center></h1>`;
+      document.getElementById('question-number').style.display = 'none';
+      firstopt.style.display = 'none';
+      secondopt.style.display = 'none';
+      thirdopt.style.display = 'none';
+      fourthopt.style.display = 'none';
+      nextQuestion.style.display = 'none';
+      document.getElementById('endgame').style.display = 'block';
+      document.getElementById('resultbox').style.display = 'none';
+      document.getElementById('showresult').style.display = 'block';
+  document.getElementById('showresult').textContent = `Your Score is ${correctAnswer} out of ${questions.length}`;
+      document.getElementById('playagain').style.display = 'block'; 
+    }
+}
+
+//loadNext Question on clicking nextbtn
+function loadNextQuestion() {
+    loadQuestion(currentQuestion);
+    currentQuestion++;
+    if(currentQuestion === totalQuestion) {
+      nextQuestion.textContent = 'End Game';
+    }
+    enableButton();
+}
+
+// check for clicked options 
+function check(e) {
+    if(e.id == questions[questionIndex].correctAnswer) {
+      correctAnswer++;
+        e.className = "correct";
+        e.innerHTML = "Correct Answer";
+        resultCounter.textContent = correctAnswer;
+    }else {
+      e.className = "wrong";
+      e.innerHTML = "Wrong Answer";
+    }
+    disableButton();
+}
+
+//disable and enable options
+function disableButton() {
+  for (let i = 0; i < option.children.length; i++) {
+      option.children[i].style.pointerEvents = 'none';
+      if(option.children[i].id == questions[questionIndex].correctAnswer) {
+          option.children[i].className = "correct"; }
+    }
+}
+function enableButton() {
+  for (let i = 0; i < option.children.length; i++) {
+    option.children[i].style.pointerEvents = 'auto';
+    option.children[i].className = ''; }
+}
+
+window.onload = function() {
+  loadNextQuestion();
+}
+
+
+
+
+
+
+
